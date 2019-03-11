7d3342221ebb127c09a3c85ce87a55d698640ab1
zhangnan <zhangnan@zhiketong.cn>
2019-03-11 14:35:52 +0800

    add config

--- template/src/main.js
+++ template/src/main.js
@@ -1,6 +1,24 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
+{{#mock}}
+import './mocks'
+{{/mock}}
+
+Vue.use(VueResource)
+Vue.http.options.root = '//gateway.com'
+Vue.http.options.crossOrigin = true
+Vue.http.options.xhr = {withCredentials: true}
+Vue.http.headers.common.Authorization = localStorage.token
+
+Vue.http.graphql = function (query, variables) {
+  var data = {
+    query,
+    variables
+  }
+  return Vue.http.post.call(this, 'graphql', data)
+}
 
 new Vue({
   el: '#app',
