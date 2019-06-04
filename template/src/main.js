5c8bb12eaf0e4f947b175942dacb48c7fa2afa0e
zhangnan <zhangnan@zhiketong.cn>
2019-06-04 16:42:46 +0800

    增加本地环境

--- template/src/main.js
+++ template/src/main.js
@@ -1,3 +1,29 @@
+import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App.vue'
+import store from './store'
+import 'bootstrap/dist/css/bootstrap.css'
 
-export default App
+Vue.use(VueResource)
+Vue.http.options.root = process.env.GATEWAY_URL
+Vue.http.options.crossOrigin = true
+Vue.http.options.xhr = {withCredentials: true}
+Vue.http.headers.common.Authorization = localStorage.token
+
+Vue.http.graphql = function (query, variables) {
+  var data = {
+    query,
+    variables
+  }
+  return Vue.http.post.call(this, 'graphql', data)
+}
+
+new Vue({
+  el: '#app',
+  store,
+  render: h => h(App)
+})
+
+// import App from './App.vue'
+
+// export default App
